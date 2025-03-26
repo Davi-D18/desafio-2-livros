f1d8dcf5d8e3ca68694e824fc3f3c9a0b06a7985
Davi-D18 <luizdavi.code@gmail.com>
2025-03-26 10:25:08 -0300

    adicionado configuração de CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,8 @@
 import os
 from flask import Flask
-
+from flask_cors import CORS
 from .routes.routes import main as main_blueprint
-
-
 from .extensions import db, migrate
-
-
 from .config.config import config
 
 def create_app():
@@ -15,14 +11,8 @@ def create_app():
     env = os.getenv('FLASK_ENV', 'development')
     app.config.from_object(config[env])
     
-    
-    
-    
     db.init_app(app)
     migrate.init_app(app, db)
-    
-
-    
     
     # Registro das rotas
     app.register_blueprint(main_blueprint)
